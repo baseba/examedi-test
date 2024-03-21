3bf578b4ca8e46dc0a8764e52d83f18f92a9c882
baseba <sebastin.espejo@uc.cl>
2024-03-21 01:26:02 -0300

    init

---
+++ app/[pokemon]/page.tsx
@@ -0,0 +1,53 @@
+'use client'
+
+import { useParams } from "next/navigation";
+import { useContext, useEffect, useState } from "react"
+
+
+
+type pokemon = {
+    name: string;
+    height: number;
+    id: number;
+    url: string;
+    };
+
+export default function Pokemon() {
+    const { pokemon } = useParams() as {pokemon: string};
+    const [types, setTypes] = useState([] as string[]);
+    const [sprite, setSprite] = useState('' as string);
+    const [height, setHeight] = useState(0 as number);
+    const [weight, setWeight] = useState(0 as number);
+    useEffect (() => {
+        const fetchData = async () => {
+        console.log(pokemon);
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+        const data = await response.json();
+        setTypes(data.types.map((type: { type: { name: string; }; }) => type.type.name));
+        setHeight(data.height);
+        setWeight(data.weight);
+        const formUrl = data.forms[0].url;
+        // get the api url to obtain the sprite
+        const response2 = await fetch(formUrl);
+        const data2 = await response2.json();
+        setSprite(data2.sprites.front_default);
+
+
+        };
+        fetchData();
+    }, []);
+    return (
+        <div>
+            <h1>{pokemon}</h1>
+            <img src={sprite} alt={pokemon}/>
+            <ul>
+                {types.map((type, index) => (
+                    <li key={index}>{type}</li>
+                ))}
+            </ul>
+            <h2>height: {height}m</h2>
+            <h2>weight: {weight}</h2>
+
+        </div>
+    );
+}
---
+++ app/components/card.tsx
@@ -0,0 +1,84 @@
+'use client'
+import { serializePageInfos } from "next/dist/build/utils";
+import { useEffect, useState } from "react"
+type Pokemon = {
+    name: string;
+    url: string;
+  };
+export default function PokeCard(props :Pokemon) {
+
+    const { name , url } = props;
+
+    const [sprite, setSprite] = useState('' as string);
+    const [loading, setLoading] = useState(true);
+    const [types, setTypes] = useState([] as string[]);
+    const [number, setNumber] = useState(0 as number);
+
+    const colorTypes = {
+        "normal": 'bg-gray-400',
+        "fire": 'bg-red-400',
+        "water": 'bg-blue-400',
+        "grass": 'bg-green-400',
+        "flying": 'bg-yellow-400',
+        "fighting": 'bg-orange-400',
+        "poison": 'bg-purple-400',
+        "electric": 'bg-yellow-400',
+        "ground": 'bg-brown-400',
+        "rock": 'bg-gray-400',
+        "psychic": 'bg-pink-400',
+        "ice": 'bg-blue-400',
+        "bug": 'bg-green-400',
+        "ghost": 'bg-purple-400',
+        "steel": 'bg-gray-400',
+        "dragon": 'bg-red-400',
+        "dark": 'bg-gray-400',
+        "fairy": 'bg-pink-400',
+    };
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch(url);
+            const data = await response.json();
+            setNumber(data.id);
+            const formUrl = data.forms[0].url;
+            // get the api url to obtain the sprite
+            const response2 = await fetch(formUrl);
+            const data2 = await response2.json();
+            setSprite(data2.sprites.front_default);
+            setTypes(data2.types.map((type: { type: { name: string; }; }) => type.type.name));
+            setLoading(false);
+            // console.log(data);
+
+            
+        };
+
+        fetchData();
+    }, []);
+
+    return (
+        loading? <div className=" bg-slate-400 w-3 animate-spin"></div> :
+        <div className="border-white border-4 bg-slate-500 rounded-md min-h-80 min-w-60 flex flex-col items-center">
+            
+            <div className="flex items-center p-4 ">
+            <img
+                alt={name}
+                className="aspect-square object-cover rounded-lg border bg-slate-400"
+                height={150}
+                src={sprite}
+                width={150}
+            />
+            </div>
+            <div className="flex items-start p-4">
+                <div className="flex flex-col">
+                    <h2 className="text-lg font-bold leading-none">{name}</h2>
+                    <h3 className=" text-gray-200"># {number}</h3>
+                    {types.map((t) => (
+                        <p className={`text-sm font-semibold p-1 my-1 rounded leading-none ${colorTypes[t as keyof typeof colorTypes]}`}>{t}</p>
+                    ))}
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
+
